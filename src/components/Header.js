739dae7b0103951d0dd1591e10b4951958c8b537
kayashth1 <yashprof2004@gmail.com>
2024-06-01 15:22:32 +0530

    Some minor changes done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from "react"
 import "./style.css"
+import img from "../Images/trollface_PNG48.png"
 export default function Header() {
     return (
         <header className="header">
-            <img className="header--image" alt="header-img" src="./images/trollface_PNG48" />
-            <h2 className="header--title">Header component</h2>
-            <h4 className="header--project">React Course - Project 3</h4>
+            <img className="header--image" alt="header-img" src={img} />
+            <h2 className="header--title">Meme Website</h2>
+            <h4 className="header--project">Yash Shrivastva</h4>
         </header>
     )
 }
